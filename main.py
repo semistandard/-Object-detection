53acdf1371e751fc1351ee98756bdb0a7db8d6dc
semistandard <71646868+semistandard@users.noreply.github.com>
2021-12-13 01:24:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ endpoint = "https://20211212tetsuya.cognitiveservices.azure.com/"
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 
 ## 画像タグの取得
-def get_tags(local_image):
-    # local_image = open(filepath, "rb")
+def get_tags(filepath):
+    local_image = open(filepath, "rb")
     tags_result = computervision_client.tag_image_in_stream(local_image)
     tags = tags_result.tags
     tags_name = []
@@ -24,8 +24,8 @@ def get_tags(local_image):
         tags_name.append(tag.name)
     return list(tags_name)
 
-def detect_objects(local_image):
-    # local_image = open(filepath, "rb")
+def detect_objects(filepath):
+    local_image = open(filepath, "rb")
 
     detect_objects_results = computervision_client.detect_objects_in_stream(local_image)
     objects = detect_objects_results.objects
@@ -41,8 +41,8 @@ uploaded_file = st.file_uploader('Choose an image...',type=['jpg','png','jpeg','
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
-    img_path=f'./{uploaded_file.name}'
-    objects = detect_objects(img)
+    img_path=f'{uploaded_file.name}'
+    objects = detect_objects(img_path)
     
     #描画
     draw = ImageDraw.Draw(img)
@@ -62,7 +62,7 @@ if uploaded_file is not None:
 
     st.image(img)
 
-    tags_name = get_tags(img)
+    tags_name = get_tags(img_path)
     tags_name = ', '.join(tags_name)
 
     st.markdown('**認識されたコンテンツタグ**')
