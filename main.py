f6ebf3275cf8cc27a44dda1bab24d23e92efc8c8
semistandard <71646868+semistandard@users.noreply.github.com>
2021-12-13 23:55:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,35 +41,39 @@ st.title('物体検出アプリ')
 uploaded_file = st.file_uploader('Choose an image...',type=['jpg','png','jpeg'])
 
 IMG_PATH = 'path/to'
+import tempfile
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
     st.image(img)
-    img_path = os.path.join(IMG_PATH, uploaded_file.name)
+#     img_path = os.path.join(IMG_PATH, uploaded_file.name)
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        fp = Path(tmp_file.name)
+        fp.write_bytes(uploaded_file.getvalue())
 
-    st.write(f'{img_path}')
-    objects = detect_objects(img_path)
-    
-    #描画
-    draw = ImageDraw.Draw(img)
-#     for object in objects:
-#         x = object.rectangle.x
-#         y = object.rectangle.y
-#         w = object.rectangle.w
-#         h = object.rectangle.h
-#         caption = object.object_property
+        st.write(f'{img_path}')
+        objects = detect_objects(img_path)
 
-#         font = ImageFont.truetype(font='./Helvetica 400.ttf', size=50)
-#         text_w,text_h = draw.textsize(caption, font=font)
+        #描画
+        draw = ImageDraw.Draw(img)
+    #     for object in objects:
+    #         x = object.rectangle.x
+    #         y = object.rectangle.y
+    #         w = object.rectangle.w
+    #         h = object.rectangle.h
+    #         caption = object.object_property
 
-#         draw.rectangle([(x,y),(x+text_w,y+text_h)], fill='green', outline='green', width=5)
-#         draw.rectangle([(x,y),(x+w,y+h)], fill=None, outline='green', width=5)
-#         draw.text((x,y), caption,fill='white',font=font)
+    #         font = ImageFont.truetype(font='./Helvetica 400.ttf', size=50)
+    #         text_w,text_h = draw.textsize(caption, font=font)
 
-#     st.image(img)
+    #         draw.rectangle([(x,y),(x+text_w,y+text_h)], fill='green', outline='green', width=5)
+    #         draw.rectangle([(x,y),(x+w,y+h)], fill=None, outline='green', width=5)
+    #         draw.text((x,y), caption,fill='white',font=font)
 
-#     tags_name = get_tags(img_path)
-#     tags_name = ', '.join(tags_name)
+    #     st.image(img)
 
-    st.markdown('**認識されたコンテンツタグ**')
-#     st.markdown(f'> {tags_name}' )
+    #     tags_name = get_tags(img_path)
+    #     tags_name = ', '.join(tags_name)
+
+        st.markdown('**認識されたコンテンツタグ**')
+    #     st.markdown(f'> {tags_name}' )
